cb62be7e8f26453d66be701261ac5661497efbd7
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2024-09-28 17:49:49 +0200

    Insert text to display both the data value and data key name in the column chart

--- src/chartModules/columnChart.js
+++ src/chartModules/columnChart.js
@@ -34,18 +34,31 @@ export class ColumnChart extends Chart {
       const theme = this._getTheme()
 
       const columnWidth = this._canvasElement.width / amountOfColumns
-
       const maxValue = this.#getMaxDataValue()
 
-      Object.entries(this._dataPoints).forEach(([ key, data ], index) => {
+      Object.entries(this._dataPoints).forEach(([ name, data ], index) => {
         const columnHeight = (data / maxValue) * this._canvasElement.height
         const position = index * columnWidth
 
         // Cycle through all the data colors in the theme.
         chart.fillStyle = theme.data[index % theme.data.length]
 
-        // Draw the actual column.
+        // Draw the column.
         chart.fillRect(position, this._canvasElement.height - columnHeight, columnWidth - 1, columnHeight)
+
+        chart.fillStyle = theme.lines || 'black'
+        chart.textAlign = 'center'
+        chart.font = '"Roboto", sans-serif'
+
+        const dataTextX = position + columnWidth / 2
+        const dataTextY = this._canvasElement.height - columnHeight + 10
+
+        chart.fillText(data, dataTextX, dataTextY)
+
+        const nameTextX = position + columnWidth / 2
+        const nameTextY = this._canvasElement.height - 3
+
+        chart.fillText(name, nameTextX, nameTextY)
       }) 
     } catch (error) {
       this._errorHandler.consoleError(error)
